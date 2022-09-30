0f8d27a45166f09c1918a8b2c26ead167ea70eff
kahouxiu <hefangzhou412@yahoo.co.jp>
2022-09-30 23:45:35 +0900

    my commit

--- src/main/java/com/example/demo/MyFirstApp1Application.java
+++ src/main/java/com/example/demo/MyFirstApp1Application.java
@@ -1,9 +1,14 @@
 package com.example.demo;
 
+
+
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+@MapperScan("com.example.demo.dao")
 public class MyFirstApp1Application {
 
 	public static void main(String[] args) {
